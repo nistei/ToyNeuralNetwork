dd07d5fa54ad33585b1896a98b538f66318f8171
Niklas Steiner <niklas@sbg.at>
2018-04-16 21:18:01 +0200

    Added DataPair Support

---
+++ ToyNeuralNetwork/DataPair.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ToyNeuralNetwork
+{
+    public class DataPair
+    {
+        public double[] Input { get; set; }
+        public double[] Target { get; set; }
+
+        public DataPair(double[] input, double[] target)
+        {
+            Input = input;
+            Target = target;
+        }
+
+    }
+}
